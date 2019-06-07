f4c80c5d611e73cf21617a1135d5b83af5af15fa
Minal Kharche <minal.berkeley@gmail.com>
2019-06-07 01:17:00 -0700

    updated code in js file with main logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,54 +1,101 @@
 $(document).ready(function(){
 
-var topics = ["tom and Jerry","aaladin","mikey","tom and jerry"];
+    var topics = ["inside out", "popeye", "the lion king", "finding nemo"];
 
-
-
-//onclick
-$("#addButton").on("click",function(){
-    event.preventDefault();
-    var getUserInput = $("#addInput").val().trim();
-    $("#addInput").val("");
-    topics.push(getUserInput);
-    console.log("getUserInput: ",getUserInput);
-    console.log("Topics: ",topics)
     renderBtnFun();
-});
 
-// function
-function renderBtnFun(){
-    $(".renderBtn").empty();
-    //for (x in topics){ 
+    //onclick
+    $("#addButton").on("click", function () {
+        event.preventDefault();
+        var getUserInput = $("#addInput").val().trim();
+        $("#addInput").val("");
+        topics.push(getUserInput);
+        console.log("getUserInput: ", getUserInput);
+        console.log("Topics: ", topics)
+        renderBtnFun();
+    });
+
+    // function
+    function renderBtnFun() {
+        $("#renderBtn").empty();
         for (var i = 0; i < topics.length; i++) {
             var j = topics[i];
-        var display = $(".renderBtn");
-        var newBtn = $("<button>");
-        newBtn.text(j).addClass("btn btn-primary");
-        display.append(newBtn);
-    }  
-}
+            var displayInWrap = $("#renderBtn");
+            var newBtn = $("<button>");
+            //console.log(j);
+            newBtn.text(j).addClass("btnList btn btn-primary");
+            displayInWrap.append(newBtn);
+        }
+    }
+
+    $("#renderBtn").on("click", ".btnList", function () {
+        searchParam = $(this).text();
+       
+        var api = "https://api.giphy.com/v1/gifs/search?"
+        var apiKey = "&api_key=hdLZ99k0fwSXFUwI8roD0vpPcG9I0tcO"
+        var query = "&q=" + searchParam;
+        searchQuery = api + apiKey + query;
+
+        $.ajax({
+            url: searchQuery,
+            method: "GET"
+        }).then(function (response){
+            console.log(response.data);
+            displayImg(response);
+
+        });
+
+    })
+
+    function displayImg(getData) {
+
+        var displayInWrap = $("#renderGif");
+        displayInWrap.empty();
+
+        for (i = 0; i < 10; i++) {
+            //gif div created
+            var gifThumbnail = $("<div>");
+            var gifCaption = $('<div>');
+            var gifRating = getData.data[i].rating;
+
+            gifCaption.addClass('caption text-center').text("Rating : " + gifRating);
+
+            gifThumbnail.addClass('thumbnail').append(gifCaption);
+
+            var childGif = $("<img>");
+            var gifSrcStill = getData.data[i].images.fixed_height_still.url;
+            var gifSrcAnimate = getData.data[i].images.fixed_height.url;
+
+            childGif.attr('src', gifSrcStill);
+            childGif.attr("data-still", gifSrcStill)
+            childGif.attr("data-animate", gifSrcAnimate)
+            childGif.attr("data-state", "still")
+            childGif.addClass("gifState");
+            gifThumbnail.append(childGif);
+
+            displayInWrap.append(gifThumbnail);
+
+        }
+    }
+
+    $("#renderGif").on("click", ".thumbnail img", function () {
+        console.log(" Image clicked");
+
+        var getGifState = $(this).attr("data-state");
 
 
+        if (getGifState == "still") {
+            var getAnimateUrl = $(this).attr("data-animate")
+            $(this).attr("data-state", "animate");
+            $(this).attr("src", getAnimateUrl);
 
+        } else {
+            var getStillUrl = $(this).attr("data-still")
+            $(this).attr("data-state", "still");
+            $(this).attr("src", getStillUrl);
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    });
 
 
 });
